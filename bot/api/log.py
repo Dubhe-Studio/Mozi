33befa5f755ebd61ce5fee56d39bc9ce49110d26
Gu_ZT <administration@ztxy666.cn>
2021-11-29 19:42:54 +0800

    Fixed can not create logs dir

--- bot/api/log.py
+++ bot/api/log.py
@@ -8,6 +8,9 @@ def Log(text: str = None, sender: str = '控制台'):
     asia_dt = utc_dt.astimezone(datetime.timezone(datetime.timedelta(hours=8)))
     log_text = asia_dt.strftime('[%Y-%m-%d %H:%M:%S]') + ' <' + sender + '> ' + text
     file = os.path.join(pro_dir + '\\logs\\latest.log')
+    file_dir = os.path.join(pro_dir + '\\logs')
+    if not os.path.isdir(file_dir):
+        os.mkdir(file_dir)
     if os.path.isfile(file):
         file_handle = open(file, mode='a', encoding="utf-8")
         file_handle.write(log_text + '\n')
