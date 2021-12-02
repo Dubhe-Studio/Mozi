a644c0849b05608f0c2ab965cb05394c84b884f6
Gu_ZT <administration@ztxy666.cn>
2021-12-02 19:48:55 +0800

    Update root.py

--- plugins/MCPlus/root.py
+++ plugins/MCPlus/root.py
@@ -25,27 +25,30 @@ def onStart():
     async def server_command(msg: Message, address: str = "0"):
         await msg.reply(getMcServer.getServer(address))
 
-    def is_op(msg: Message, *args) -> bool:
+    def is_op(msg: Message) -> bool:
         return msg.author.id == admin_id
 
-    def is_enable_channel(msg: Message, *args) -> bool:
+    def is_enable_channel(msg: Message) -> bool:
         return msg.ctx.channel.id in channel_id
 
-    @bot.command(name='enable_use', rules=[is_op])
+    @bot.command(name='enable_use')
     async def enable_use_command(msg: Message):
-        print(is_op(msg))
-        if msg.ctx.channel.id not in channel_id:
-            channel_id.append(msg.ctx.channel.id)
-            pluginJson.writeJson('MCPlus', 'config', 'channels', channel_id)
-        await msg.reply(f"{msg.ctx.channel.id}已加入允许的频道列表")
-        log.INFO(f"{msg.ctx.channel.id}已加入允许的频道列表", "MCPlus")
+        if is_op(msg):
+            print(is_op(msg))
+            if msg.ctx.channel.id not in channel_id:
+                channel_id.append(msg.ctx.channel.id)
+                pluginJson.writeJson('MCPlus', 'config', 'channels', channel_id)
+            await msg.reply(f"{msg.ctx.channel.id}已加入允许的频道列表")
+            log.INFO(f"{msg.ctx.channel.id}已加入允许的频道列表", "MCPlus")
 
-    @bot.command(name='wladd', rules=[is_enable_channel])
+    @bot.command(name='wladd')
     async def wladd_command(msg: Message, name: str):
-        await msg.reply(runCommand.whitelistAdd(name))
+        if is_enable_channel(msg):
+            await msg.reply(runCommand.whitelistAdd(name))
 
-    @bot.command(name='seed', rules=[is_enable_channel])
+    @bot.command(name='seed')
     async def seed_command(msg: Message):
-        await msg.reply(runCommand.seed())
+        if is_enable_channel(msg):
+            await msg.reply(runCommand.seed())
 
     log.INFO("插件已载入", "MCPlus")
